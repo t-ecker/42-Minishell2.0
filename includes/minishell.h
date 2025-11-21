93c3cbb062b6098bf2ee262239ed46c1072e745a
t-ecker <152566019+t-ecker@users.noreply.github.com>
2025-11-21 16:15:20 +0100

    add minimal shell struct

--- includes/minishell.h
+++ includes/minishell.h
@@ -4,5 +4,23 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
+# include <stdbool.h>
+
+# include "../libft/libft.h"
+# include "errors.h"
+
+typedef struct s_shell
+{
+	char *input;
+	int exit_code;
+	char *env	// env can be other type idk
+
+} t_shell;
+
+
+void free_shell(t_shell *shell);
+int input_validation(t_shell *shell);
+void exit_code(t_shell *shell, int code);
+// void free_env(idk)
 
 # endif
