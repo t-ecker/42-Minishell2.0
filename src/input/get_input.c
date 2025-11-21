71af90f3906ef1ee4694a224d94be4d97a2934cc
t-ecker <152566019+t-ecker@users.noreply.github.com>
2025-11-21 16:15:44 +0100

    add get_input function

---
+++ src/input/get_input.c
@@ -0,0 +1,11 @@
+# include "minishell.h"
+
+int get_input(t_shell *shell)
+{
+	shell->input = readline("minishell: ");
+	if (!shell->input)
+		return (1);
+	if (shell->input[0] != '\0')
+		add_history(shell->input);
+	return (0);
+}
