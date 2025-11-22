fa732dc1b7ca0999678a8a69caba1eeee91f2adf
t-ecker <152566019+t-ecker@users.noreply.github.com>
2025-11-22 01:25:19 +0100

    add input redirection validation

---
+++ src/input/validate_redirection.c
@@ -0,0 +1,37 @@
+# include "../../includes/minishell.h"
+
+int handle_redir_syntax(char *str, int *pos)
+{
+	int i;
+	char c;
+
+	i = *pos;
+	c = str[i];
+	if (str[i + 1] == c)
+		i++;
+	i++;
+	skip_spaces(str, &i);
+	if (!str[i])
+		return (syntax_error(SYNTAX_REDIR_EOF_ERROR), 1);
+	if (is_special_char(str[i]))
+		return (syntax_error_unexpected_token(str[i]), 1);
+	*pos = i - 1;
+	return(0);
+}
+
+int check_redir(char *str)
+{
+	int pos;
+
+	pos = 0;
+	while(str[pos])
+	{
+		skip_quotes(str, &pos);
+		if (str[pos] == '>' || str[pos] == '<')
+			if (handle_redir_syntax(str, &pos))
+				return (1);
+		if (str[pos])
+			pos++;
+	}
+	return (0);
+}
