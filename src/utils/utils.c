03be33b5e0400c730ebcb8848422477a9bab9709
t-ecker <152566019+t-ecker@users.noreply.github.com>
2025-11-22 01:24:37 +0100

    add more utils function needed for input validation

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -5,3 +5,43 @@ void exit_code(t_shell *shell, int code)
 {
 	shell->exit_code = code;
 }
+
+bool is_special_char(char c) {
+    return c == '|' || c == '&' || c == '(' || c == ')' || 
+           c == '<' || c == '>';
+}
+
+void skip_quotes(char *str, int *pos)
+{
+	char quote;
+
+	if (str[*pos] == '\'' || str[*pos] == '"')
+	{
+		quote = str[(*pos)++];
+		while(str[*pos] && str[*pos] != quote)
+			(*pos)++;
+		if (str[*pos] == quote)
+            (*pos)++;
+	}
+}
+
+#include <ctype.h> //correct LIBFT function for isspace
+
+void skip_spaces(char *str, int *pos)
+{
+	while(str[*pos] && isspace(str[*pos])) 
+		(*pos)++;
+}
+
+void toggleBool(bool *input)
+{
+	*input = !(*input);
+}
+
+void toggle_quote(char c, bool *openDoubleQuote, bool *openSingleQuote)
+{
+	if (c == '\'' && !*openDoubleQuote)
+		toggleBool(openSingleQuote);
+	else if (c == '"' && !*openSingleQuote)
+		toggleBool(openDoubleQuote);
+}
