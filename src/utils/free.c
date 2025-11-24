09d7d050ba07539990ea5622ffcff835c0dc828b
t-ecker <152566019+t-ecker@users.noreply.github.com>
2025-11-24 23:17:00 +0100

    add token freeing

--- src/utils/free.c
+++ src/utils/free.c
@@ -5,6 +5,24 @@ void free_shell(t_shell *shell)
 {
 	if (shell->input)
 		free(shell->input);
+	if (shell->tokens)
+		free_tokens(shell->tokens);
+}
+
+void free_tokens(t_tokenList *tokens)
+{
+    t_token *current;
+    t_token *next;
+	
+	current = tokens->head;
+    while (current)
+	{
+        next = current->next;
+        free(current->value);
+        free(current);
+        current = next;
+    }
+    free(tokens);
 }
 
 // void free_env(idk)
