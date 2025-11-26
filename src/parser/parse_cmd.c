62b0c4cd02bbb4cd0016f30373286741e8ed741e
t-ecker <152566019+t-ecker@users.noreply.github.com>
2025-11-26 22:27:59 +0100

    add cmd and redir parsing

---
+++ src/parser/parse_cmd.c
@@ -0,0 +1,56 @@
+# include "../../includes/minishell.h"
+
+t_argList *create_args_node(char *value, t_parser *p)
+{
+	t_argList *node;
+	
+	node = gc_malloc(p->shell, sizeof(t_argList));
+	node->value = gc_add(p->shell, ft_strdup(value));
+	node->next = NULL;
+	return (node);
+}
+
+void add_arg_node(t_argList **head, char *value, t_parser *p)
+{
+	t_argList *node;
+	t_argList *current_node;
+	
+	node = create_args_node(value, p);
+	if (*head == NULL)
+		*head = node;
+	else
+	{
+		current_node = *head;
+		while(current_node->next)
+			current_node = current_node->next;
+		current_node->next = node;
+	}
+}
+
+bool is_redir_token(t_tokenType type)
+{
+	return (type == TOKEN_APPEND_OUT || type == TOKEN_HEREDOC 
+			|| type == TOKEN_REDIRECT_IN || type == TOKEN_REDIRECT_OUT);
+}
+
+// until there is something else than a redir or word token parse into one cmd node that has an args list and an redir list
+// redirList stores all redirs in this block after another.
+// argList stores all args after another
+
+t_astNode *parse_cmd(t_parser *p)
+{
+	t_astNode *command_node;
+
+	command_node = create_astNode(AST_COMMAND, p);
+	while (p->current_token && p->current_token->type != TOKEN_EOF)
+	{
+		if (is_redir_token(p->current_token->type))
+			handle_redirection(&command_node->u_data.command.redirects, p);
+		else if (p->current_token->type == TOKEN_WORD)
+			add_arg_node(&command_node->u_data.command.args, p->current_token->value, p);
+		else
+			break ;
+		advance_token(p);
+	}
+	return (command_node);
+}
---
+++ src/parser/parse_redir.c
@@ -0,0 +1,51 @@
+# include "../../includes/minishell.h"
+
+t_redirectList *create_redir_node(t_redirectType type, char *target, t_parser *p)
+{
+	t_redirectList *node;
+	
+	node = gc_malloc(p->shell, sizeof(t_redirectList));
+	node->type = type;
+	node->target = gc_add(p->shell, ft_strdup(target));
+	node->next = NULL;
+	return (node);
+}
+
+void add_redir_node(t_redirectList **head, t_redirectType type, char *target, t_parser *p)
+{
+	t_redirectList *node;
+	t_redirectList *current_node;
+	
+	node = create_redir_node(type, target, p);
+	if (*head == NULL)
+		*head = node;
+	else
+	{
+		current_node = *head;
+		while(current_node->next)
+			current_node = current_node->next;
+		current_node->next = node;
+	}
+}
+
+t_redirectType get_redir_type(t_tokenType type)
+{
+	if (type == TOKEN_APPEND_OUT)
+		return (REDIR_APPEND);
+	if (type == TOKEN_HEREDOC)
+		return (REDIR_HEREDOC);
+	if (type == TOKEN_REDIRECT_IN)
+		return (REDIR_INPUT);
+	else
+		return (REDIR_OUTPUT);
+}
+
+void handle_redirection(t_redirectList **head, t_parser *p)
+{
+	t_redirectType redir_type;
+
+	redir_type = get_redir_type(p->current_token->type);
+	advance_token(p);
+	// check if next is word
+	add_redir_node(head, redir_type, p->current_token->value, p);
+}
