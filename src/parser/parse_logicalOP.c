81256100db36ed6094cf9a8cffe844cfcfb25364
t-ecker <152566019+t-ecker@users.noreply.github.com>
2025-11-26 22:28:49 +0100

    add logicalOperator parsing

---
+++ src/parser/parse_logicalOP.c
@@ -0,0 +1,31 @@
+# include "../../includes/minishell.h"
+
+// this is the start. first it parses left side of logical op (cmd (redir)(pipe))
+// then if there is a logical operator it parses right side.
+// combiens left and right into a logical node with the correct op_type
+
+t_astNode *parse_logical(t_parser *p)
+{
+	t_astNode *left;
+	t_astNode *right;
+	t_astNode *logical_node;
+	t_logicalOpType op_type;
+
+	left = parse_pipe(p);
+	while(check_token_type(p, TOKEN_AND) || check_token_type(p, TOKEN_OR))
+	{
+		if (p->current_token->type == TOKEN_AND)
+			op_type = LOG_OP_AND;
+		else
+			op_type = LOG_OP_OR;
+		advance_token(p);
+		right = parse_pipe(p);
+		// maybe error check
+		logical_node = create_astNode(AST_LOGICAL_OP, p);
+		logical_node->u_data.logical_op.op_type = op_type;
+		logical_node->u_data.logical_op.left = left;
+		logical_node->u_data.logical_op.right = right;
+		left = logical_node;
+	}
+	return (left);
+}
