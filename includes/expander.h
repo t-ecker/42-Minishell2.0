7f5fbff178a0bd8fcd955be4097ac23cb98c589d
t-ecker <152566019+t-ecker@users.noreply.github.com>
2025-11-27 19:47:08 +0100

    add env var expander

---
+++ includes/expander.h
@@ -0,0 +1,19 @@
+#ifndef EXPANDER_H
+# define EXPANDER_H
+
+typedef struct s_expand
+{
+	char *res;
+	bool insideSingleQuote;
+	bool insideDoubleQuote;
+	size_t capacity;
+	t_shell *shell;
+} t_expand;
+
+void expander(char **str, t_shell *shell);
+char *expand_var(char *str, t_shell *shell);
+t_expand *create_expander(t_shell *shell);
+void append_str(char *str, t_expand *e);
+void append_char(char c, t_expand *e);
+
+#endif
---
+++ src/expander/expand_variable.c
@@ -0,0 +1,66 @@
+# include "../../includes/minishell.h"
+
+int extract_var_name(char *start, char **var_name, t_shell *shell)
+{
+	int len;
+
+	len = 0;
+	while(start[len] && (ft_isalnum(start[len]) || start[len] == '_'))
+		++len;
+	if (len == 0)
+		return (0);
+	*var_name = gc_malloc(shell, len + 1);
+	ft_memcpy(*var_name, start, len);
+	(*var_name)[len] = '\0';
+	return (len);
+}
+
+void handle_var(char *str, int *pos, t_expand *e)
+{
+	char *var_name;
+	int var_len;
+	char *value;
+
+	var_len = extract_var_name(&str[*pos], &var_name, e->shell);
+	if (var_len > 0)
+	{
+		value = get_env_var(var_name, e->shell);
+		if (value)
+			append_str(value, e);
+		*pos += var_len;
+	}
+	else
+		append_char('$', e);
+}
+
+char *expand_var(char *str, t_shell *shell)
+{
+	int pos;
+	t_expand *e;
+
+	pos = 0;
+	e = create_expander(shell);
+	while (str[pos])
+	{
+		if (str[pos] == '\'' || str[pos] == '"')
+		{
+			toggle_quote(str[pos], &e->insideDoubleQuote, &e->insideSingleQuote);
+			append_char(str[pos], e);
+		}
+		else if (str[pos] == '$' && !e->insideSingleQuote)
+		{
+			++pos;
+			if (str[pos] == '?')
+				append_str(gc_add(shell, ft_itoa(shell->exit_code)), e);
+			else
+			{
+				handle_var(str, &pos, e);
+				continue;
+			}
+		}
+		else
+			append_char(str[pos], e);
+		++pos;
+	}
+	return (e->res);
+}
