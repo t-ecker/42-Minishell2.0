a7bceaafa772ba017dd0cc51b1a5372cfbc84ca2
t-ecker <152566019+t-ecker@users.noreply.github.com>
2025-12-09 12:12:03 +0100

    fix exit to handle exit code overflow

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -1,13 +1,5 @@
 # include "../../includes/minishell.h"
 
-void message(char *str)
-{
-	ft_putendl_fd("exit", 1);
-	ft_putstr_fd("minishell: exit: ", 2);
-	ft_putstr_fd(str, 2);
-	ft_putendl_fd(": numeric argument required", 2);
-}
-
 long ft_atol(char *str)
 {
     long sign = 1;
@@ -25,6 +17,33 @@ long ft_atol(char *str)
     return result * sign;
 }
 
+bool would_overflow(char *arg)
+{
+	int i;
+	int sign;
+	int len;
+	char *relevant_part;
+
+	i = 0;
+	sign = 1;
+	if (arg[i] == '+' || arg[i] == '-')
+	{
+		if (arg[i] == '-')
+			sign = -1;	
+		++i;
+	}
+	while(arg[i] == '0')
+		++i;
+	relevant_part = &arg[i];
+	len = ft_strlen(relevant_part);
+	if (len > 19)
+		return (true);
+	if (len < 19)
+		return (false);
+	if (sign == 1)
+		return (ft_strncmp(relevant_part, "9223372036854775807", 19) > 0);
+	return (ft_strncmp(relevant_part, "9223372036854775808", 19) > 0);
+}
 
 int check_arg(char *arg)
 {
@@ -34,13 +53,15 @@ int check_arg(char *arg)
 	if (arg[0] == '+' || arg[0] == '-')
 		++i;
 	if (!arg[i])
-		return(message(arg), 255);
+		return(exit_error_numeric(arg), 2);
 	while(arg[i])
 	{
 		if (!ft_isdigit(arg[i]))
-			return(message(arg), 2);
+			return(exit_error_numeric(arg), 2);
 		++i;
 	}
+	if (would_overflow(arg))
+		return(exit_error_numeric(arg), 2);
 	return ((unsigned char)ft_atol(arg));
 }
 
@@ -58,6 +79,8 @@ int ft_exit(char **args, t_shell *shell)
 		else
 			shell->exit_code = check_arg(args[1]);
 	}
+	else
+		ft_putendl_fd("exit", 1);
 	free_all(shell);
 	exit(shell->exit_code);
 }
--- src/utils/errors.c
+++ src/utils/errors.c
@@ -32,6 +32,13 @@ void export_error(char c)
 	ft_putendl_fd("': not a valid identifier", 2);
 }
 
+void exit_error_numeric(char *str)
+{
+	ft_putendl_fd("exit", 1);
+	ft_putstr_fd("minishell: exit: ", 2);
+	ft_putstr_fd(str, 2);
+	ft_putendl_fd(": numeric argument required", 2);
+}
 
 //needs to free everything at any time
 void fatal_error(t_shell *shell, char *msg)
