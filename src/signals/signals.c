3169e85219ee2f9e39ddf9a618b7f3d1d164a0d0
t-ecker <152566019+t-ecker@users.noreply.github.com>
2025-12-11 16:20:19 +0100

    norm proof signals

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -1,8 +1,8 @@
-# include "../../includes/minishell.h"
+#include "../../includes/minishell.h"
 
-volatile sig_atomic_t g_signal_received;
+volatile sig_atomic_t	g_signal_received;
 
-void sigint_main_handler(int sig)
+void	sigint_main_handler(int sig)
 {
 	(void)sig;
 	if (g_signal_received != SIGINT)
@@ -12,46 +12,45 @@ void sigint_main_handler(int sig)
 	rl_redisplay();
 }
 
-void heredoc_signal_handler(int sig)
+void	heredoc_signal_handler(int sig)
 {
-    g_signal_received = sig;
+	g_signal_received = sig;
 	write(1, "\n", 1);
 	close(STDIN_FILENO);
 }
 
-void disable_ctrl_c_echo(void)
+void	disable_ctrl_c_echo(void)
 {
-    struct termios term;
-    
-    tcgetattr(STDIN_FILENO, &term);
-    term.c_lflag &= ~ECHOCTL;
-    tcsetattr(STDIN_FILENO, TCSANOW, &term);
+	struct termios	term;
+
+	tcgetattr(STDIN_FILENO, &term);
+	term.c_lflag &= ~ECHOCTL;
+	tcsetattr(STDIN_FILENO, TCSANOW, &term);
 }
 
-void enable_ctrl_c_echo(void)
+void	enable_ctrl_c_echo(void)
 {
-    struct termios term;
-    
-    tcgetattr(STDIN_FILENO, &term);
-    term.c_lflag |= ECHOCTL;
-    tcsetattr(STDIN_FILENO, TCSANOW, &term);
+	struct termios	term;
+
+	tcgetattr(STDIN_FILENO, &term);
+	term.c_lflag |= ECHOCTL;
+	tcsetattr(STDIN_FILENO, TCSANOW, &term);
 }
 
-void setup_main_signals(void)
+void	setup_main_signals(void)
 {
 	disable_ctrl_c_echo();
 	signal(SIGINT, sigint_main_handler);
 	signal(SIGQUIT, SIG_IGN);
 }
 
-void setup_child_signals(void)
+void	setup_child_signals(void)
 {
-	// enable_ctrl_c_echo();
 	signal(SIGINT, SIG_DFL);
 	signal(SIGQUIT, SIG_DFL);
 }
 
-void setup_heredoc_signals(void)
+void	setup_heredoc_signals(void)
 {
 	signal(SIGINT, heredoc_signal_handler);
 }
